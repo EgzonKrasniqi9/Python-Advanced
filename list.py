1fbd51560c8b20c7f816badd95d2840ec25be2d8
EgzonKrasniqi9 <egzon2008krasniqi1@gmail.com>
2026-02-10 17:48:10 +0100

    comit

---
+++ list.py
@@ -0,0 +1,15 @@
+to_do_list=['Play','Learn','Train','Eat']
+first_item=to_do_list[0]
+second_item=to_do_list[1]
+third_item=to_do_list[2]
+fourth_item=to_do_list[3]
+
+print(first_item)
+print(second_item)
+print(third_item)
+print(fourth_item)
+
+shopping_list=['aple', 'bananas', 4 , 3.60]
+shopping_list.append('orange')
+
+print(shopping_list)
---
+++ variable.py
@@ -0,0 +1,17 @@
+temperature = 42.5
+name = ('Egzon')
+
+print(type(temperature))
+print(type(name))
+
+age = 17
+
+print("age is :", 17)
+
+age=age+1
+print("age now is :", age)
+
+first_name='Egzon'
+last_name='Krasniqi'
+full_name=first_name+" "+last_name
+print(full_name)
