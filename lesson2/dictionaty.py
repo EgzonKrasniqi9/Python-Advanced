ff0baa81392d887c74138c2dff2811dfdc2e8d0d
EgzonKrasniqi9 <egzon2008krasniqi1@gmail.com>
2026-02-19 17:18:51 +0100

    comit

--- lesson2/dictionaty.py
+++ lesson2/dictionaty.py
@@ -21,3 +21,35 @@ print(values)
 
 items = contact_info.items()
 print(items)
+
+contact_information = {
+
+    "Festa":{
+        "phone_number" : "123-456",
+        "email" : "sara@gmail.com",
+        "home_address" : "Bregu i Diellit",
+        "birthday" : "24/12/2007"
+    },
+    "Melina":{
+        "phone_number" : "789-465",
+        "email" : "melina@gmail.com",
+        "home_address" : "Taslixhe",
+        "birthday" : "27/09/2008"
+    }
+}
+
+print(contact_information)
+
+sara_information = contact_information["Festa"]
+print(sara_information)
+
+contacts = {
+    "Festa":("123-456", "festa@gmail.com"),
+    "Melina":("789-456", "melina@gmail.com"),
+}
+
+festa_info = contacts["Festa"]
+phone_number = festa_info[0]
+print(phone_number)
+
+phone_number, email = contacts["Festa"]
---
+++ lesson2/touples_dictionaries.py
@@ -0,0 +1,9 @@
+grades = {
+    ("Melina", "Math"): 2,
+    ("Dion", "Physicis"): 3.5,
+    ("Festa", "Biology"): 5,
+    ("Reina", "English"): 4,
+}
+
+melina_math = grades[("Melina", "Math")]
+print("Melina's grade in math is", melina_math)
